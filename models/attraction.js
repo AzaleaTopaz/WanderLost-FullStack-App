8322eb58250641ff0acd7767285bd845b1305e90
AzaleaTopaz <azaleahowland@gmail.com>
2024-06-02 13:47:21 -0400

    created attraction model

---
+++ models/attraction.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const  { Schema } = require('mongoose')
+
+const Attraction = new Schema(
+    {
+        name: {type: String, required: true },
+        priceUsd: {type: Number, required: true },
+        type: {type: String, required: true},
+        description: {type: String, required: true},
+        city:{type: mongoose.Schema.Types.ObjectId, ref: 'city'}
+        
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('attraction', Attraction)
