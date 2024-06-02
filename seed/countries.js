627e4485396220fa2a3f4e6fa329de11c48cd050
AzaleaTopaz <azaleahowland@gmail.com>
2024-06-02 14:55:47 -0400

    populated countries seed

--- seed/countries.js
+++ seed/countries.js
@@ -7,6 +7,62 @@ const main = async () => {
 
     const countries = [
 
+        {
+            name: 'Japan',
+            continent: 'Asia',
+            time_zone: '(GMT+9)' ,
+            language: ['Japanese']
+        },
+
+        {
+            name: 'Costa Rica',
+            continent: 'Noth America',
+            time_zone: '(GMT-6)' ,
+            language: ['Spanish']
+        },
+
+        {
+            name: 'Peru',
+            continent: 'South America',
+            time_zone: '(GMT-5)' ,
+            language: ['Spanish, Quechuan, Aymara']
+        },
+
+        {
+            name: 'Mexico',
+            continent: 'North America',
+            time_zone: '(GMT-6)' ,
+            language: ['Spanish']
+        },
+
+        {
+            name: 'United States',
+            continent: 'North America',
+            time_zone: '(GMT-4)' ,
+            language: ['English']
+        },
+
+        {
+            name: 'South Korea',
+            continent: 'Asia',
+            time_zone: '(GMT-4)' ,
+            language: ['Korean']
+        },
+        {
+            name: 'Thailand',
+            continent: 'Asia',
+            time_zone: '(GMT+7)' ,
+            language: ['Thai']
+        },
+        
+        {
+            name: 'Botswana',
+            continent: 'Africa',
+            time_zone: '(GMT+2)' ,
+            language: ['English, Setswana, Ikalanga']
+        },
+
+
     ]
 
     await Country.insertMany(countries)
