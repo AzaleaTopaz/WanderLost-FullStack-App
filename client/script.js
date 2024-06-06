712db1c514418960e8dd9679a39fe2d84a15d013
AzaleaTopaz <azaleahowland@gmail.com>
2024-06-06 16:04:57 -0400

    fixed variables

--- client/script.js
+++ client/script.js
@@ -1,13 +1,20 @@
 const bodyEl = document.querySelector('body');
-const destinationContainer = getElementById('destinationContainer');
-const imageContainer = getElementById('imageContainer');
-const cityInfoContainer = getElementById('cityInfoContainer');
-const attractionInfoContainer = getElementById('attractionInfoContainer');
+const destinationContainer = document.getElementById('destinationContainer');
+const imageContainer = document.getElementById('imageContainer');
+const cityInfoContainer = document.getElementById('cityInfoContainer');
+const attractionInfoContainer = document.getElementById('attractionInfoContainer');
 const headingEl = document.querySelector('h1')
 const button = document.getElementById('enter')
+const selectEl = document.createElement('select')
 
 let countries
 let cities
 let attractions
 
-// create 
+// add an eventlistener to button:
+button.addEventListener('click', async () => {
+    // when button is clicked, a select element will populate
+    selectEl.setAttribute('id', 'countrySelect');
+
+
+});
